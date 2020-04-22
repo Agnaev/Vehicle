3ed410e6d7c6c8f1a89bb3574e79e7a560017171
HeilungLifa <Black8192water@gmail.com>
2020-04-22 16:32:36 +0300

    Update client view

--- Receiver/View/scripts/script.js
+++ Receiver/View/scripts/script.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 import ChartElement from './Chart.js'
 import ConnectStatus from './ConnectState.js'
 
@@ -8,8 +10,8 @@ document.addEventListener('DOMContentLoaded', e => new ConnectStatus())
 
 /**
  * @param {string} url Url to api.
- * @param {{[x:string]:[x:string | number]}} options Request options.
- * @param {x:any() => void} callback Unnecessary callback function which can be called after request with request result.
+ * @param {{[key:string]:string}} options Request options.
+ * @param {Function} callback Unnecessary callback function which can be called after request with request result.
  * @returns {Promise} Result from server.
  */
 const fetch_data = async (url, options = {}, callback = null) => {
@@ -23,7 +25,7 @@ const charts = [];
 fetch_data('/get_metrics', { 'method': 'post' })
 .then(data => data.map(x => charts.push(new ChartElement(x.Id, x.Name, 0, x.Value))))
 
-document.querySelector('#connect_to_server').addEventListener('click', async event => {
+document.querySelector('#connect_to_vehicle').addEventListener('click', async event => {
     let iterator = 1;
     const web_socket_port = await fetch_data('/get_socket_port', {}, x => x.port)
     if(!web_socket_port){
@@ -33,15 +35,8 @@ document.querySelector('#connect_to_server').addEventListener('click', async eve
     const state = new ConnectStatus();
     const webSocket = new WebSocket('ws://localhost:' + web_socket_port);
     
-    webSocket.onopen = e => {
-        console.log('open web socket', e);
-        state.connect();
-    }
-
-    webSocket.onclose = e => {
-        console.log('close web socket', e);
-        state.disconnect();
-    }
+    webSocket.onopen = state.connect;
+    webSocket.onclose = state.disconnect;
 
     webSocket.onmessage = responce => {
         const data = JSON.parse(responce.data);
@@ -50,7 +45,7 @@ document.querySelector('#connect_to_server').addEventListener('click', async eve
             const curr_val = data.filter(x => x.Id === chart.Id)[0].value;
             chart.add_data(iterator++, curr_val);
         })
-        document.querySelector('#counter').textContent = iterator;
+        document.querySelector('#counter').textContent = iterator.toString();
     }
 
     document.querySelector('#close_connection')
