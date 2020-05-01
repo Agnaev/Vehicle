da46cf012ea4ee037d368f8a88249c504643fa3e
HeilungLifa <Black8192water@gmail.com>
2020-05-01 21:58:45 +0300

    update data generator

--- Emulator/data_generator.js
+++ Emulator/data_generator.js
@@ -5,47 +5,36 @@
  * @param {{min:number, max:number}} param0 object containing minimum and maximum values 
  * @returns {number} Random value
  */
-const rand = ({ min, max }) => Math.floor(Math.abs(Math.random()) * (max - min) + min);
+const rand = ({ min, max }) => Math.floor(Math.random() * (max - min) + min);
 
 /** Каррирование функции, т.е. превращение фцнкции из вида f(a, b, c) в f(a)(b)(c)
  * @param {Function} func функция которую надо каррировать*/
 function Currying(func) {
-    return function curried(...args) {
-        if (args.length >= func.length) {
-            return func.apply(this, args);
-        }
-        else {
-            return function (...args2) {
-                return curried.apply(this, [...args, ...args2]);
-            }
-        }
+    return function curried() {
+        return arguments.length >= func.length 
+            ? func.apply(this, arguments)
+            : (...args) => curried.apply(this, [...arguments, ...args]);
     }
 }
 
 /** Непосредственно функция генерации новых данных
- * @param {Array<{Id: number, Name: string, MinValue: number, MaxValue: number}>} types 
- * @param {{ init:true, [key:number]:number }} last_res
+ * @param {Array<{Id: number, Name: string, MinValue: number, MaxValue: number}>} types метрики из базы данных
+ * @param {{ init:true, [key:number]:number }} last_res последний результат работы функции
  */
-const data_generator = (types, last_res) => {
-    return types.reduce((result, { Id, MinValue, MaxValue }) => {
-        let range;
-        if (last_res.init) {
-            range = {
-                min: MinValue,
-                max: MaxValue
-            };
-        }
-        else {
-            const min = last_res[Id] - 5 < MinValue ? MinValue : last_res[Id] - 5;
-            const max = last_res[Id] + 5 > MaxValue ? MaxValue : last_res[Id] + 5;
-            range = { min, max };
-        }
-        if (Id in result) {
-            throw new Error(`Duplicate Error. Key ${Id} already defined in accumulator object.`);
-        }
-        result[Id] = rand(range);
-        return result;
-    }, {});
-};
+const data_generator = (types, last_res) => types.reduce(
+    (result, { Id, MinValue, MaxValue }) => ({
+        ...result,
+        [Id]: (() => rand(
+            last_res.init
+                ? {
+                    min: MinValue,
+                    max: MaxValue
+                }
+                : {
+                    min: last_res[Id] - 5 < MinValue ? MinValue : last_res[Id] - 5,
+                    max: last_res[Id] + 5 > MaxValue ? MaxValue : last_res[Id] + 5
+                }
+        ))()
+    }), {});
 
 module.exports.generator = Currying(data_generator);
--- Emulator/emulator.js
+++ Emulator/emulator.js
@@ -1,23 +1,23 @@
 // @ts-check
-'use strict'
+'use strict';
 
 const { Server: WebSocketServer }    = require('ws');
 const { web_socket_port: port }      = require('./config');
 const { Observer }                   = require('./Observer');
-const { makeRequest }                = require('./db_request')
-const { generator }                  = require('./data_generator')
+const { makeRequest }                = require('./db_request');
+const { generator }                  = require('./data_generator');
 
 const webSocketServer = new WebSocketServer({
-    port
+    port 
 });
 
 (async () => {
-    const data_from_db = await makeRequest('select * from MetricsTypes');
-    const types = data_from_db.recordsets[0];
+    const { recordsets: [types] } = await makeRequest('select * from MetricsTypes');
 
     const observer = new Observer(generator(types));
 
-    webSocketServer.on('connection', socket => {
+    webSocketServer.on('connection', (socket, req) => {
+        console.log(`User with ip: ${req.socket.remoteAddress} was connected.`);
         const unsubscribe = observer.subscribe(
             /**@param {string} data JSON string value*/
             data => socket.send(data)
